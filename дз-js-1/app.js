e4bc070adf414ebd684e4b4161cd5aa553101c16
Eugene Bocharov <bocharovwork12@gmail.com>
2021-01-22 12:20:48 +0200

    js

---
+++ дз-js-1/app.js
@@ -0,0 +1,42 @@
+'use strict'
+
+//     1
+
+// string
+
+const str = 'привіт';
+
+console.log(typeof str);
+
+// number
+
+const num = 123;
+
+console.log(typeof num);
+
+// boolean
+
+const flag = true;
+
+console.log(typeof flag);
+
+// string
+
+const txt = 'true';
+
+console.log(typeof txt);
+
+//     2
+
+let num1 = 245;
+let num2 = 245;
+let num3 = 245;
+let num4 = 245;
+let num5 = 245;
+
+const str1 = 'text';
+const str2 = 'text';
+const str3 = 'text';
+const str4 = 'text';
+const str5 = 'text';
+const str6s = 'text';
---
+++ дз-js-2/js/script.js
@@ -0,0 +1,88 @@
+'use strict';
+
+//  1
+
+
+let user_name = prompt('Ваше ім\'я?', "Name");
+
+console.log(user_name);
+
+
+//  2
+
+
+const mage = 12;
+const fage = 13;
+
+alert(mage + fage);
+
+
+//  3
+
+
+const a = 2;
+const b = 3;
+
+console.log(a + b);
+
+
+//  4
+
+
+let a1 = 5 + 3;
+let a2 = 5 - 3;
+let a3 = 5 * 3;
+let a4 = 5 / 3;
+let a5 = 5 % 3;
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
+
+
+// 5
+
+
+let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 + '3';
+let a9 = '5' - 3;
+let a10 = 75 + 'кг';
+
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+alert(a10);
+
+//  6
+
+
+const height = 23;
+const width = 10;
+
+let s = height * width;
+
+console.log(s);
+
+//  7
+
+
+let heightC = 10;
+let DC = 4;
+
+let v = 3.14 * ((DC / 2) ** 2) * heightC;
+
+console.log(v);
+
+//  8
+
+
+const n = 3;
+const m = 4;
+
+let k = (((n ** 2) + (m ** 2)) ** 0.5);
+
+console.log(k);
